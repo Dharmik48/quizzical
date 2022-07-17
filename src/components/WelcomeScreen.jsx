75aeef9d2ec50ff109b2650135f0c3c04e9b3a5c
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-17 17:23:45 +0530

    complete welcomescreen component

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,13 +1,16 @@
-function WelcomeScreen() {
+function WelcomeScreen({ setShowWelcome }) {
 	return (
-		<div className='h-full bg-bg-blue flex flex-col gap-4 items-center justify-center'>
+		<div className='h-full flex flex-col gap-4 items-center justify-center'>
 			<h1 className='text-text-blue font-karla text-3xl md:text-6xl'>
 				Quizzical
 			</h1>
 			<p className='text-text-blue font-inter md:text-xl'>
 				Test your knowledge!
 			</p>
-			<button className='text-white bg-btn-blue font-inter px-6 py-2 mt-4 rounded-md shadow-xl cursor-pointer transition-all hover:opacity-80 active:scale-90 focus:opacity-80 md:text-xl md:px-12 md:py-4 md:rounded-lg'>
+			<button
+				className='text-white bg-btn-blue font-inter px-6 py-2 mt-4 rounded-md shadow-xl cursor-pointer transition-all hover:opacity-80 active:scale-90 focus:opacity-80 md:text-xl md:px-12 md:py-4 md:rounded-lg'
+				onClick={() => setShowWelcome(false)}
+			>
 				Start Quiz
 			</button>
 		</div>
