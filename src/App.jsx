b5871e9e161fbb4bbede08abe7ea5fd8caa4fee4
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-17 15:01:41 +0530

    add welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from 'react';
+import WelcomeScreen from './components/WelcomeScreen';
+import yellowBlob from './images/yellow-blob.svg';
+import blueBlob from './images/blue-blob.svg';
 
 function App() {
-	return <h1 className='color-red'>Hello World!</h1>;
+	return (
+		<div className='h-screen relative overflow-hidden'>
+			<img
+				src={yellowBlob}
+				className='absolute top-0 right-0 -rotate-12 translate-x-1/2 -translate-y-1/3 scale-125 sm:scale-90'
+				alt='yellow blob'
+			/>
+			<img
+				src={blueBlob}
+				className='absolute bottom-0 left-0 -translate-x-1/3 translate-y-1/2 scale-125 sm:scale-90'
+				alt='yellow blob'
+			/>
+			<WelcomeScreen />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/WelcomeScreen.jsx
@@ -0,0 +1,17 @@
+function WelcomeScreen() {
+	return (
+		<div className='h-full bg-bg-blue flex flex-col gap-4 items-center justify-center'>
+			<h1 className='text-text-blue font-karla text-3xl md:text-6xl'>
+				Quizzical
+			</h1>
+			<p className='text-text-blue font-inter md:text-xl'>
+				Test your knowledge!
+			</p>
+			<button className='text-white bg-btn-blue font-inter px-6 py-2 mt-4 rounded-md shadow-xl cursor-pointer transition-all hover:opacity-80 active:scale-90 focus:opacity-80 md:text-xl md:px-12 md:py-4 md:rounded-lg'>
+				Start Quiz
+			</button>
+		</div>
+	);
+}
+
+export default WelcomeScreen;
