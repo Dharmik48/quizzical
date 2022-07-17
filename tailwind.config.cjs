34d1f13c733e900794b448aaf8c0fd5c01bd7832
Dharmik_403 <hingudharmik4@gmail.com>
2022-07-17 12:52:58 +0530

    add assests

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			karla: ['Karla', 'sans-serif'],
+			inter: ['inter', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'bg-blue': '#F5F7FB',
+				'btn-blue': '#4D5B9E',
+				'text-blue': '#293264',
+				'bg-text': '#D6DBF5',
+				'correct-green': '#94D7A2',
+				'wrong-red': '#F8BCBC',
+			},
+		},
 	},
 	plugins: [],
 };
