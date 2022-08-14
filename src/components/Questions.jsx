22f4a3d055e4462b2c7fd6b0212029fc8ed26750
kabilansakthivelu <kabilansakthi97@gmail.com>
2022-08-14 15:04:44 +0530

    Place 'Back' button at the top of the page

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -67,7 +67,15 @@ function Questions({ url, setShowModal, setShowWelcome }) {
   return (
     <div className="z-10 h-full max-w-6xl flex flex-col justify-center items-start gap-3 p-8 sm:px-16 lg:gap-8 lg:py-16">
       {questions?.length ? (
-        renderQuestions()
+        <>
+          <button
+            className="self-start text-white bg-btn-blue font-inter px-6 py-2 rounded-md shadow-xl cursor-pointer transition-all hover:opacity-80 active:scale-90 focus:opacity-80 md:text-xl md:px-12 md:py-4 md:rounded-lg"
+            onClick={goBack}
+          >
+          Back
+          </button>
+          {renderQuestions()}
+        </>
       ) : (
         <p className="text-md font-karla text-text-blue md:text-xl lg:text-2xl self-center">
           Loading Quiz...
@@ -99,12 +107,6 @@ function Questions({ url, setShowModal, setShowWelcome }) {
                 Check Answers
               </button>
             )}
-            <button
-                className="self-center text-white bg-btn-blue font-inter px-6 py-2 rounded-md shadow-xl cursor-pointer transition-all hover:opacity-80 active:scale-90 focus:opacity-80 md:text-xl md:px-12 md:py-4 md:rounded-lg"
-                onClick={goBack}
-              >
-                Back
-              </button>
           </>
         )}
       </div>
